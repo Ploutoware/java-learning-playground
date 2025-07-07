01bff8594f2815a16b053f404b355a771d216c2a
Plousieleex <a-xy.cnbrk@hotmail.com>
2025-07-07 21:11:28 +0300

    Challenge1 part2

--- src/autoboxunbox/challenge/Main.java
+++ src/autoboxunbox/challenge/Main.java
@@ -17,6 +17,18 @@ public class Main
     {
         Customer bob = new Customer("Bob S", 1000.0);
         System.out.println(bob);
+
+        Bank bank = new Bank("Chase");
+        bank.addNewCustomer("Jane A", 500.0);
+        System.out.println(bank);
+
+        bank.addTransaction("Jane A", -10.25);
+        bank.addTransaction("jane A", -75.01);
+        bank.printStatement("Jane A");
+
+        bank.addNewCustomer("Bob s", 25);
+        bank.addTransaction("Bob S", 100);
+        bank.printStatement("Bob S");
     }
 }
 
@@ -28,6 +40,14 @@ class Bank
     public Bank(String name)
     {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Bank{" +
+                "name='" + name + '\'' +
+                ", customers=" + customers +
+                '}';
     }
 
     private Customer getCustomer(String customerName)
@@ -51,6 +71,32 @@ class Bank
             Customer customer = new Customer(customerName, initialDeposit);
             customers.add(customer);
             System.out.println("New Customer added: " + customer);
+        }
+    }
+
+    public void addTransaction(String name, double transactionAmount)
+    {
+        Customer customer = getCustomer(name);
+        if (customer != null)
+        {
+            customer.transactions().add(transactionAmount);
+        }
+    }
+
+    public void printStatement(String customerName)
+    {
+        Customer customer = getCustomer(customerName);
+        if (customer == null)
+        {
+            return;
+        }
+
+        System.out.println("_".repeat(30));
+        System.out.println("Customer name: " + customer.name());
+        System.out.println("Transactions ");
+        for (double d : customer.transactions())
+        {
+            System.out.printf("$%10.2f (%s)%n", d, d < 0 ? "debit" : "credit");
         }
     }
 }
