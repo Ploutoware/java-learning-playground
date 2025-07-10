8ed4fb73625a9130de02c3fb1ada4a3bf52eaa94
Plousieleex <a-xy.cnbrk@hotmail.com>
2025-07-10 16:58:10 +0300

    Abstract class challenge part1

---
+++ src/abstracts/challenge1/Store.java
@@ -0,0 +1,27 @@
+package abstracts.challenge1;
+
+import java.util.ArrayList;
+
+public class Store
+{
+    private static ArrayList<ProductForSale> storeProducts = new ArrayList<>();
+    public static void main(String[] args)
+    {
+        storeProducts.add(new ArtObject("Oil Painting", 1350,
+                "Impressionistic work by ABF painted in 2010"));
+
+        storeProducts.add(new ArtObject("Sculpture", 2000,
+                "Bronze work by JKF, procued in 1950."));
+
+        listProducts();
+    }
+
+    public static void listProducts()
+    {
+        for (var item : storeProducts)
+        {
+            System.out.println("_".repeat(30));
+            item.showDetails();
+        }
+    }
+}
