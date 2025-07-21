a828631b95cf42f65a922e05c35cf888a737a7b8
Plousieleex <a-xy.cnbrk@hotmail.com>
2025-07-21 16:51:42 +0300

    Interfaces part1

---
+++ src/interfaces/part1/Bird.java
@@ -0,0 +1,34 @@
+package interfaces.part1;
+
+public class Bird extends Animal implements FlightEnabled, Trackable
+{
+    @Override
+    public void move()
+    {
+        System.out.println("Bird move.");
+    }
+
+    @Override
+    public void takeOff()
+    {
+        System.out.println(getClass().getSimpleName() + " is taking off.");
+    }
+
+    @Override
+    public void land()
+    {
+        System.out.println(getClass().getSimpleName() + " is landing.");
+    }
+
+    @Override
+    public void fly()
+    {
+        System.out.println(getClass().getSimpleName() + " is flying.");
+    }
+
+    @Override
+    public void track()
+    {
+        System.out.println(getClass().getSimpleName() + "'s coordinates recorded.");
+    }
+}
---
+++ src/interfaces/part1/Test.java
@@ -0,0 +1,21 @@
+package interfaces.part1;
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        inFlight(new Jet());
+    }
+
+    private static void inFlight(FlightEnabled flier)
+    {
+        flier.takeOff();
+        flier.transition(FlightStages.LAUNCH);
+        flier.fly();
+        if (flier instanceof Trackable trackable)
+        {
+            trackable.track();
+        }
+        flier.land();
+    }
+}
