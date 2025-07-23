1af27e26dcef7e8eeb2acfa3bdf5370f23876649
Plousieleex <a-xy.cnbrk@hotmail.com>
2025-07-23 12:58:41 +0300

    Final

--- src/interfaces/part1/Test.java
+++ src/interfaces/part1/Test.java
@@ -5,6 +5,10 @@ public class Test
     public static void main(String[] args)
     {
         inFlight(new Jet());
+
+        // OrbitEarth.log("Testing " + new Satellite());
+
+        orbit(new Satellite());
     }
 
     private static void inFlight(FlightEnabled flier)
@@ -16,6 +20,13 @@ public class Test
         {
             trackable.track();
         }
+        flier.land();
+    }
+
+    private static void orbit(OrbitEarth flier)
+    {
+        flier.takeOff();
+        flier.fly();
         flier.land();
     }
 }
